252c59045d6a6f7cd66c362dcd39208482fe8fa5
Kevin Jaffrezou <kevin56620@gmail.com>
2021-06-17 22:53:39 +0200

    remove duplicate h1

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
         <section class="text-gray-600 body-font">
             <div class="container px-5 py-24 mx-auto">
                 <div class="flex flex-col text-center w-full mb-20">
-                    <h1 class="c-h2 u-fz-medium">Fonctionnement de la base de données</h1>
+                    <h2 class="c-h2 u-fz-medium">Fonctionnement de la base de données</h2>
                 </div>
                 <div class="flex flex-wrap -m-4">
                     <div class="p-4 md:w-1/3">
