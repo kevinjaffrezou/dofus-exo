9dd795a746f1255774df43608942387694af873d
Kevin Jaffrezou <kevin56620@gmail.com>
2021-06-17 21:51:26 +0200

    fix purge tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  purge: [
-    './components/**/*.{vue,js}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}'
-  ],
+  purge: {
+    content: [
+      `components/**/*.{vue,js}`,
+      `layouts/**/*.vue`,
+      `pages/**/*.vue`,
+      `plugins/**/*.{js,ts}`,
+      `nuxt.config.{js,ts}`,
+      'node_modules/tv-*/dist/tv-*.umd.min.js'
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -13,8 +16,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
-  content: [
-    './node_modules/tv-toast/**/*.vue'
-  ]
+  plugins: []
 }
