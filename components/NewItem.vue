459c498e49f8ff059fcb43aa975456c2964c3136
Kevin Jaffrezou <kevin56620@gmail.com>
2022-04-05 02:45:27 +0200

    update NewItem - add keyboard control and spinner

--- components/NewItem.vue
+++ components/NewItem.vue
@@ -9,13 +9,16 @@
             </div>
             <div class="c-addon-container flex-1 md:mx-9 u-margin-t-20-md-down">
                 <span class="c-icon icon-search_black_24dp"></span>
-                <input v-model="searchInput" type="text" class="c-input" placeholder="Rechercher un item Dofus">
-                <ul class="c-selectbox" v-if="items.length">
-                    <li v-for="(item, index) in items" :key="index" @click="selectItem(item)">
-                        <img width="36" height="36" :src="item.img" alt="" loading="lazy">
-                        {{ item.name }}
-                    </li>
-                </ul>
+                <span class="c-spinner" v-if="isLoading"></span>
+                <input ref="search" v-model="searchInput" type="text" class="c-input" placeholder="Rechercher un item Dofus" @focus="isSearchbarFocused = true" @blur="isSearchbarFocused = false">
+                <div>
+                    <ul class="c-selectbox" v-if="items.length && isSearchbarFocused">
+                        <li :class="{'is-focus': items[focusedItem] == item}" v-for="(item, index) in items" :key="index" @mousedown="selectItem(item)">
+                            <img width="36" height="36" :src="item.img" alt="" loading="lazy">
+                            {{ item.name }}
+                        </li>
+                    </ul>
+                </div>
             </div>
             <TheButton class="--primary u-margin-t-20-md-down" @click.native="addItem(selectedItem)">Ajouter</TheButton>
         </div>
@@ -28,8 +31,11 @@ import _ from "lodash"
 export default {
     data() {
         return {
+            isLoading: false,
             searchInput: '',
             items: [],
+            focusedItem: null,
+            isSearchbarFocused: false,
             selectedItem: {
                 img: '/img/default-item.png'
             }
@@ -43,17 +49,48 @@ export default {
             }
         }
     },
+    mounted() {
+        this.$refs.search.focus()
+        document.addEventListener('keydown', this.keyboardAccessibility);
+    },
+    destroyed() {
+        document.removeEventListener('keydown', this.keyboardAccessibility);
+    },
     created() {
         this.debouncedGetItems = _.debounce(this.getItems, 500)
     },
     methods: {
+        keyboardAccessibility: function(event) {
+            switch (event.key) {
+                case 'ArrowDown':
+                    if(this.focusedItem === null || this.focusedItem >= this.items.length -1) {
+                        this.focusedItem = 0
+                    } else {
+                        this.focusedItem++
+                    }
+                    break;
+                case 'ArrowUp':
+                    if(this.focusedItem === null || this.focusedItem === 0) {
+                        this.focusedItem = this.items.length - 1
+                    } else {
+                        this.focusedItem--
+                    }
+                    break;
+                case 'Enter':
+                    if(this.focusedItem) this.selectItem(this.items[this.focusedItem])
+                    if(this.searchInput !== '' && this.focusedItem === null) this.addItem(this.selectedItem)
+                    break;
+            }
+        },
         getItems: async function () {
             let _this = this
             this.items = [] // Clear items table
+            this.focusedItem = null // reset focusedItem index
             this.selectedItem = {img: '/img/default-item.png'} // Remove current search
             if(this.searchInput === '' ) return
-
-            const equipements = await this.$http.$get(`https://dofus-api.vercel.app/api?name=${this.searchInput}&cannotFm=false`)
+            this.isLoading = true
+            const equipements = await this.$http.$get(`https://dofus-api.vercel.app/api?name=${this.searchInput}&cannotFm=false&limit=6`)
+            this.isLoading = false
             
             equipements.forEach(element => {
                 _this.items.push({
@@ -62,46 +99,10 @@ export default {
                     img: `/img/items/${element.imageId}.png`,
                     type: element.type
                 })
-            });
-            
-
-
-            // const acceptedType = [
-            //     "Anneau",
-            //     "Amulette",
-            //     "Bottes",
-            //     "Bouclier",
-            //     "Cape",
-            //     "Ceinture",
-            //     "Chapeau",
-            //     "Sac à dos",
-            //     "Arc",
-            //     "Baguette",
-            //     "Bâton",
-            //     "Dague",
-            //     "Faux",
-            //     "Hache",
-            //     "Marteau",
-            //     "Pelle",
-            //     "Épée"
-            // ];
-
-            // let _this = this
-            // this.dofusEquiments.forEach(element => {
-            //     if(element.name.toLowerCase().includes(this.searchInput.toLowerCase())) {
-            //         if(acceptedType.includes(element.type)) {
-            //             _this.items.push({
-            //                 dofusId: element._id,
-            //                 name: element.name,
-            //                 img: '/img/items/'+element.imgUrl.split('/').slice(-1).pop(),
-            //                 type: element.type
-            //             })
-            //         }
-                    
-            //     }
-            // })
+            })
         },
         selectItem: function (item) {
+            this.focusedItem = null // reset focusedItem index
             this.selectedItem = item
             this.searchInput = item.name
             this.items = []
